0c0fe2d6b2b0929eac6f69fafab7786a79a7aa2e
Xiling Huang <wisherhxl@gmail.com>
2023-05-31 13:35:23 +0800

    feature: search for sources automatically

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -1,16 +1,23 @@
-set(sources
-    src/tmp.cpp
+
+file(GLOB_RECURSE exe_sources CONFIGURE_DEPENDS
+  "${CMAKE_CURRENT_SOURCE_DIR}/src/*.c"
+  "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp"
 )
 
-set(exe_sources
-		src/main.cpp
-		${sources}
+
+set(sources "")
+foreach(source ${exe_sources})
+  if(NOT ${source} MATCHES "${CMAKE_CURRENT_SOURCE_DIR}/src/main.*")
+    list(APPEND sources ${source})
+  endif()
+endforeach()
+
+file(GLOB_RECURSE headers CONFIGURE_DEPENDS 
+  "${CMAKE_CURRENT_SOURCE_DIR}/include/*.h"
+  "${CMAKE_CURRENT_SOURCE_DIR}/include/*.hpp"
 )
 
-set(headers
-    include/project/tmp.hpp
-)
-
-set(test_sources
-  src/tmp_test.cpp
+file(GLOB_RECURSE test_sources CONFIGURE_DEPENDS 
+  "${CMAKE_CURRENT_SOURCE_DIR}/test/src/*.c"
+  "${CMAKE_CURRENT_SOURCE_DIR}/test/src/*.cpp"
 )
